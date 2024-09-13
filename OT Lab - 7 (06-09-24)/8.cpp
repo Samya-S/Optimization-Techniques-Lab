e1efba209044faea52bae09fbe0b9701bede4a5b
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2024-09-13 21:59:36 +0530

    feat: Asg 8 checkAlternativeSolution

--- OT Lab - 7 (06-09-24)/8.cpp
+++ OT Lab - 7 (06-09-24)/8.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
-#include <vector>
+
 #include <iomanip>
 #include <limits>
+#include <vector>
 
 using namespace std;
-
+int fl = 0;
 class Simplex {
 private:
     int n, m, artificialCount;
@@ -14,16 +15,14 @@ private:
     vector<double> CB;
     vector<int> B;
     vector<double> delta;
-    const double M = 1e2;  
-     int extravar = 0;
+    const double M = 1e6;
+    int extravar = 0;
     set<int> arti_ind;
     vector<int> isunrestriced;
     vector<int> neg;
 
 public:
-    Simplex(int variables, int constraints) : n(variables), m(constraints),
-                                              A(constraints, vector<double>(variables + constraints+constraints+variables)),
-                                              c(variables + constraints + constraints+variables), // c includes slack and artificial variables
+    Simplex(int variables, int constraints) : n(variables), m(constraints), A(constraints, vector<double>(variables + constraints + constraints + variables)), c(variables + constraints + constraints + variables),  // c includes slack and artificial variables
                                               b(constraints),
                                               CB(constraints),
                                               B(constraints),
@@ -33,9 +32,9 @@ public:
                                               artificialCount(0) {}
 
     void readInput() {
-        cout<<"1 for maximization problem and 0 for minimization problem"<<endl;
-        int fl;
-        cin>>fl;
+        cout << "1 for maximization problem and 0 for minimization problem" << endl;
+        // int fl;
+        cin >> fl;
         cout << "Enter the coefficients of the objective function: ";
         for (int i = 0; i < n; i++) {
             cin >> c[i];
@@ -47,100 +46,112 @@ public:
                 cin >> A[i][j];
             }
         }
-        cout<<"enter 0 for restricted variable and 1 for unrestricted variable"<<endl;
-        for(int i = 0 ;i<n;i++) cin>>isunrestriced[i];
-        
+        cout << "enter 0 for restricted variable and 1 for unrestricted variable" << endl;
+        for (int i = 0; i < n; i++) cin >> isunrestriced[i];
+
+        if (fl == 0) {
+            for (int i = 0; i < c.size(); i++) c[i] *= -1;
+        }
+
         vector<string> sign(m);
-        for(int i = 0 ;i<m;i++) cin>>sign[i];
+        cout << "Enter <=/>=/= : ";
+        for (int i = 0; i < m; i++) cin >> sign[i];
 
         cout << "Enter the right-hand side of the constraints: ";
         for (int i = 0; i < m; i++) {
             cin >> b[i];
             if (b[i] < 0) {
-                b[i]*=-1;
-                for(int j = 0 ;j<n;j++){
-                    A[i][j]*=-1;
+                b[i] *= -1;
+                for (int j = 0; j < n; j++) {
+                    A[i][j] *= -1;
                 }
-                if(sign[i]==">=") sign[i] = "<=";
-                else if(sign[i]=="<=") sign[i] = ">=";
+                if (sign[i] == ">=")
+                    sign[i] = "<=";
+                else if (sign[i] == "<=")
+                    sign[i] = ">=";
             }
         }
         int cnt = 0;
-        for(int i = 0 ;i<m;i++){
-            if(sign[i]!="<=") cnt++;
+        for (int i = 0; i < m; i++) {
+            if (sign[i] != "<=")
+                cnt++;
         }
-        if(cnt==0){
-            cout<<"simplex method will be used here"<<endl;
+        if (cnt == 0) {
+            cout << "simplex method will be used here" << endl;
+        } else {
+            cout << "big m method is used here " << endl;
         }
-        else {
-            cout<<"big m method is used here "<<endl;
-        }
-
 
         // Set up slack and artificial variables
-       
+
         for (int i = 0; i < m; i++) {
             string constraintType = sign[i];
-           
 
             if (constraintType == "<=") {
                 // Slack variable
-                A[i][n + extravar] = 1.0;   // Slack variable coefficient
-                c[n + extravar] = 0.0;      // Slack variable in the objective function
-                B[i] = n + extravar;        // Slack variables start as basic variables
-                CB[i] = 0.0;         // Coefficient of slack variable in the objective
-                extravar++;         
-            }
-            else if (constraintType == "=") {
+                A[i][n + extravar] = 1.0;  // Slack variable coefficient
+                c[n + extravar] = 0.0;     // Slack variable in the objective function
+                B[i] = n + extravar;       // Slack variables start as basic variables
+                CB[i] = 0.0;               // Coefficient of slack variable in the objective
+                extravar++;
+            } else if (constraintType == "=") {
                 // Artificial variable
                 A[i][n + extravar] = 1.0;  // Artificial variable coefficient
-                c[n + extravar] = -M;       // Penalty M in objective function
+                c[n + extravar] = -M;      // Penalty M in objective function
                 B[i] = n + extravar;       // Artificial variable starts as basic
                 CB[i] = -M;                // Artificial variable in the basis
                 artificialCount++;
-                arti_ind.insert(n+extravar);
+                arti_ind.insert(n + extravar);
                 extravar++;
-            }
-            else if (constraintType == ">=") {
+            } else if (constraintType == ">=") {
                 // Surplus variable
                 A[i][n + extravar] = -1.0;  // Surplus variable coefficient
                 c[n + extravar] = 0.0;      // Surplus variable in objective function
                 extravar++;
                 // Artificial variable
-                A[i][n +extravar] = 1.0;  // Artificial variable coefficient
-                c[n + extravar] = -M;       // Penalty M in objective function
+                A[i][n + extravar] = 1.0;  // Artificial variable coefficient
+                c[n + extravar] = -M;      // Penalty M in objective function
                 B[i] = n + extravar;       // Artificial variable starts as basic
-                CB[i] = -M;                 // Artificial variable in the basis
+                CB[i] = -M;                // Artificial variable in the basis
                 artificialCount++;
-                arti_ind.insert(n+extravar);
+                arti_ind.insert(n + extravar);
                 extravar++;
             }
         }
-
-        for(int i = 0 ;i<m;i++){
-            for(int j = 0 ;j<n;j++){
-
-                if(isunrestriced[j]==1){
-                    A[i][n + extravar] = -1.0*A[i][j];  
-                    c[n + extravar] =-1.0 *c[j];   
-                    neg[j] = n+ extravar;      
-                    extravar++; 
+        vector<int> tmp_extra(n, 0);
+        int tmp_ex = extravar;
+        for (int i = 0; i < n; i++) {
+            if (isunrestriced[i]) {
+                tmp_extra[i] = n + tmp_ex;
+                tmp_ex++;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isunrestriced[j] == 1) {
+                    A[i][tmp_extra[j]] = -1.0 * A[i][j];
+                    c[tmp_extra[j]] = -1.0 * c[j];
+                    neg[j] = tmp_extra[j];
                 }
             }
         }
+        extravar = tmp_ex;
 
-        if(fl==0){
-            for(int i = 0 ;i<c.size();i++) c[i]*=-1;
-        }
+        // if (fl == 0) {
+        //     for (int i = 0; i < c.size(); i++) c[i] *= -1;
+        // }
     }
 
     void computeDelta() {
-        for (int j = 0; j < n +extravar; j++) {
+        for (int j = 0; j < n + extravar; j++) {
             double sum = 0.0;
             for (int i = 0; i < m; i++) {
                 sum += CB[i] * A[i][j];
             }
+
             delta[j] = sum - c[j];
+            // if (j == 3)
+            //     cout << c[j] << endl;
         }
     }
 
@@ -179,7 +190,7 @@ public:
         double pivotElement = A[leaving][entering];
 
         // Normalize pivot row
-        for (int j = 0; j < n +extravar; j++) {
+        for (int j = 0; j < n + extravar; j++) {
             A[leaving][j] /= pivotElement;
         }
         b[leaving] /= pivotElement;
@@ -188,7 +199,7 @@ public:
         for (int i = 0; i < m; i++) {
             if (i != leaving) {
                 double factor = A[i][entering];
-                for (int j = 0; j < n +extravar; j++) {
+                for (int j = 0; j < n + extravar; j++) {
                     A[i][j] -= factor * A[leaving][j];
                 }
                 b[i] -= factor * b[leaving];
@@ -209,10 +220,11 @@ public:
         cout << '	' << "b" << endl;
 
         for (int i = 0; i < m; i++) {
-            
-            cout << '	' << "X" << B[i] + 1 << '	' ;
-            if(CB[i]==-100) cout<<"-M";
-            else cout<<CB[i];
+            cout << '	' << "X" << B[i] + 1 << '	';
+            if (CB[i] == -100)
+                cout << "-M";
+            else
+                cout << CB[i];
             for (int j = 0; j < n + extravar; j++) {
                 cout << '	' << A[i][j];
             }
@@ -220,10 +232,46 @@ public:
         }
 
         cout << "Δj:		";
-        for (int j = 0; j < n +extravar; j++) {
+        for (int j = 0; j < n + extravar; j++) {
             cout << '	' << delta[j];
         }
         cout << endl;
+    }
+
+    void checkAlternativeSolution() {
+        for (int j = 0; j < n; j++) {
+            if (delta[j] == 0 && find(B.begin(), B.end(), j) == B.end()) {
+                cout << "Alternative solution exists due to non-basic variable X" << j + 1 << " with zero reduced cost." << endl;
+                // Perform an additional pivot to compute the alternative solution
+                int entering = j;
+                int leaving = findLeavingVariable(entering);
+                if (leaving == -1) {
+                    cout << "Problem is unbounded." << endl;
+                    return;
+                }
+                pivot(entering, leaving);
+
+                // Output the alternative solution
+                vector<double> soln(n, 0.0);
+                for (int i = 0; i < m; i++) {
+                    if (B[i] < n) {
+                        double vall = b[i];
+                        int neg_ind = neg[B[i]];
+                        if (isunrestriced[B[i]] && (find(B.begin(), B.end(), neg_ind) != B.end())) {
+                            int ind = find(B.begin(), B.end(), neg_ind) - B.begin();
+                            vall -= b[ind];
+                        }
+                        soln[B[i]] = vall;
+                        // cout << "X" << B[i] + 1 << " = " << vall << endl;
+                    }
+                }
+                for (int i = 0; i < n; i++) {
+                    cout << "X" << i + 1 << " = " << soln[i] << endl;
+                }
+                return;
+            }
+        }
+        cout << "No alternative solution exists." << endl;
     }
 
     void solve() {
@@ -253,32 +301,40 @@ public:
 
         // Print final solution
         cout << "Final Solution:" << endl;
+        vector<double> soln(n, 0.0);
         for (int i = 0; i < m; i++) {
-            if(B[i]<n){
-            double vall = b[i];
-            int neg_ind = neg[B[i]];
-            if(isunrestriced[B[i]] && (find(B.begin(),B.end(),neg_ind)!=B.end())){
-                int ind = find(B.begin(),B.end(),neg_ind) - B.begin();
-                vall-=b[ind];
-            }
-            cout << "X" << B[i] + 1 << " = " << vall << endl;
-
+            if (B[i] < n) {
+                double vall = b[i];
+                int neg_ind = neg[B[i]];
+                if (isunrestriced[B[i]] && (find(B.begin(), B.end(), neg_ind) != B.end())) {
+                    int ind = find(B.begin(), B.end(), neg_ind) - B.begin();
+                    vall -= b[ind];
+                }
+                soln[B[i]] = vall;
+                //  cout << "X" << B[i] + 1 << " = " << vall << endl;
             }
         }
+        for (int i = 0; i < n; i++) {
+            cout << "X" << i + 1 << " = " << soln[i] << endl;
+        }
+
         double optimalValue = 0;
         for (int i = 0; i < m; i++) {
             optimalValue += CB[i] * b[i];
         }
+        if (fl == 0)
+            optimalValue *= -1.0;
         cout << "Optimal value of the objective function: " << optimalValue << endl;
 
         // Check for infeasibility
         for (int i = 0; i < m; i++) {
-             
-            if (arti_ind.find(B[i])!=arti_ind.end() && b[i] != 0) {
+            if (arti_ind.find(B[i]) != arti_ind.end() && b[i] != 0) {
                 cout << "Infeasible solution: artificial variable X" << B[i] + 1 << " is still in the basis." << endl;
                 return;
             }
         }
+
+        checkAlternativeSolution();
     }
 };
 
